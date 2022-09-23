4354f026e5a4f0d31549931c31356fdbf8260936
vvkveer12 <vivekyjnp2001@gmail.com>
2022-09-24 03:05:02 +0530

    update react-app

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,7 +6,7 @@ function Navbar(props) {
         <React.Fragment>
             <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/">{props.title}</a>
+                    <p className="navbar-brand">{props.title}</p>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
